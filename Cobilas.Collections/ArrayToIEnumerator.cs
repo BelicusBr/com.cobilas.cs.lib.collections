d6349f0c0b99644a37b9308aaf213b9fb01f550a
BelicusBr <X70r@outlook.com>
2023-02-20 02:03:01 -0300

    Adicionar arquivos de projeto.

---
+++ Cobilas.Collections/ArrayToIEnumerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Cobilas.Collections {
+    public class ArrayToIEnumerator<T> : IEnumerator<T> {
+        protected T[] list;
+        protected int index;
+        protected T current;
+
+        public virtual T Current => current;
+        object IEnumerator.Current => (object)current;
+
+        protected ArrayToIEnumerator() {
+            list = new T[0];
+            index = -1;
+        }
+
+        public ArrayToIEnumerator(T[] list) : this()
+            => this.list = list;
+
+        public virtual void Dispose()
+            => list = (T[])null;
+
+        public virtual bool MoveNext() {
+            if (++index >= ArrayManipulation.ArrayLength(list)) return false;
+            else current = list[index];
+            return true;
+        }
+
+        public virtual void Reset()
+            => index = -1;
+    }
+}
---
+++ Cobilas.Collections/IReadOnlyArrayGeneric.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable CS0108
+using System.Collections.Generic;
+
+namespace Cobilas.Collections {
+    public interface IReadOnlyArray<T> : IReadOnlyArray, IEnumerable<T> {
+        T this[int index] { get; }
+    }
+}
+#pragma warning restore CS0108
