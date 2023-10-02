6918a0d0a536b51cf78d4885a06c6339252bd6e6
BelicusBr <x70r@outlook.com>
2023-10-02 16:03:54 -0300

    #net35-1.0.0

---
+++ Cobilas.Collections.test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cobilas.Collections.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
